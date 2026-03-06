db2e2c2412fcf76bad542c3501b10ef34373d05f
Ben Coombs <bjcoombs@users.noreply.github.com>
2026-03-06 09:09:51 +0000

    fix: export CookbookFile type from cookbook barrel

--- frontend/src/features/cookbook/index.ts
+++ frontend/src/features/cookbook/index.ts
@@ -1,4 +1,4 @@
 export { CookbookPage } from './pages/index'
 export { CookbookDetailPage } from './pages/detail'
-export type { CookbookItem, CookbookRegistry, PatternMeta, ComponentMeta } from './hooks/use-cookbook'
+export type { CookbookItem, CookbookFile, CookbookRegistry, PatternMeta, ComponentMeta } from './hooks/use-cookbook'
 export { useCookbook } from './hooks/use-cookbook'
