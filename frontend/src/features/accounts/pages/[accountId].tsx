7653bbcfd64cfe1626f60eec827b6d84f8b36850
Ben Coombs <bjcoombs@users.noreply.github.com>
2026-03-06 12:42:09 +0000

    fix: remove unused Link import from account detail page

--- frontend/src/features/accounts/pages/[accountId].tsx
+++ frontend/src/features/accounts/pages/[accountId].tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Button } from '@/components/ui/button'
